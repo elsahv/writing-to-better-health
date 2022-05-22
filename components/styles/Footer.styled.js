7bfc1f96ae0880c33f6bb0cf72762491d33e9b19
Elsa <elsahvy@gmail.com>
2022-05-22 16:03:29 -0700

    test 4

--- components/styles/Footer.styled.js
+++ components/styles/Footer.styled.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 //STYLES
 export const FooterBanner = styled.footer`
 width: 100%;
-height: 200px;
+height: 201x;
 background: teal;
 border-top: solid 3px black;
 @media only screen and (max-width: 768px) {
