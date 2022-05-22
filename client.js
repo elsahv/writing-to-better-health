399c0cadb32045076fa8c5e6df4fe20d3a12a89d
Elsa <elsahvy@gmail.com>
2022-05-22 15:56:38 -0700

    test 3

--- client.js
+++ client.js
@@ -9,9 +9,11 @@ const config = {
    *
    * https://nextjs.org/docs/basic-features/environment-variables
    **/
-   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  apiVersion: "2022-05-22", // or today's date for latest
+   dataset: "production",
+  projectId: "abl9rf0z",
+  apiVersion: "2022-05-22", 
+  useCdn: false,
+  // or today's date for latest
 
   /**
    * Set useCdn to `false` if your application require the freshest possible
